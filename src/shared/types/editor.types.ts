6537510b41d95562016d75da658fd8c43e864438
Fr0ntcoder <jambox228@gmail.com>
2023-11-23 22:08:08 +0800

    fix/types

--- src/shared/types/editor.types.ts
+++ src/shared/types/editor.types.ts
@@ -1,6 +1,6 @@
 import { EditorProps } from 'draft-js'
 
-import { IFieldProps } from '@/shared/types/input'
+import { IFieldProps } from '@/shared/types/input.types'
 
 type TypeEditorPropsField = EditorProps & IFieldProps
 export type TTextEditor = Omit<TypeEditorPropsField, 'editorState'> & {
